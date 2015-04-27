c17664b978922f5b97d9ac17336416a066d0936b
George231086 <george231086@yahoo.co.uk>
2015-04-27 18:30:34 +0100

    Update WatchServiceExample.java

--- WatchServiceExample.java
+++ WatchServiceExample.java
@@ -9,20 +9,26 @@ import java.nio.file.WatchService;
  *
  * @author George
  */
+ 
+ // Basic example demonstrating use of java WatchService.
 public class WatchServiceExample {
     
     
     public static void main(String args[]){
+        // Get directory to watch from commandline argument.
         String directoryToWatch = args[0]; 
         Path path = Paths.get(directoryToWatch);
         WatchService watchService = null;
         try{
             watchService = path.getFileSystem().newWatchService();
+            // Link the Watchservice to the directory path. Watch for modifications, creation and deletion of
+            // files in the directory.
             path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY,
             StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
         } catch (IOException e){
             e.printStackTrace();
         }
+        
         
         while (true){
             WatchKey key = null;
