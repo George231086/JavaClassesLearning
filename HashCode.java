460bed59b9350a1a5ff6b7ed9a46529e392688f8
George231086 <george231086@yahoo.co.uk>
2015-05-07 21:55:24 +0100

    Create HashCode.java

---
+++ HashCode.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ *
+ * @author George
+ */
+public class HashCode {
+
+    public static class IceCream {
+
+        private final String flavour;
+
+        public IceCream(String flavour) {
+            this.flavour = flavour;
+
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if ((obj == null) || (obj.getClass() != this.getClass())) {
+                return false;
+            }
+            IceCream test = (IceCream) obj;
+            return (flavour.equals(test.flavour));
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 3;
+            hash = 47 * hash + Objects.hashCode(flavour);
+            return hash;
+        }
+
+    }
+
+    public static void main(String args[]) {
+        HashMap<IceCream, String> map = new HashMap<>();
+        IceCream chocolate1 = new IceCream("Chocolate");
+        map.put(chocolate1, "Yum a chocolate iceCream!");
+        IceCream chocolate2 = new IceCream("Chocolate");
+
+        // chocolate1 and chocolate2 are different instances.
+        System.out.println(chocolate1 == chocolate2); //false
+
+        // We have overridden the equals method to compare flavours.
+        System.out.println(chocolate1.equals(chocolate2)); //true
+
+        // We have overridden the hashcode method so chocolate2 acts as
+        // same key as chocolate1. If we hadn't, map.get(chocolate2) would return null
+        // even though equals returns true.
+        System.out.println(map.get(chocolate2)); // Yum a chocolate iceCream!
+    }
+}
