8495f85b6d8fa8d9d2983486820f912a651e8340
george231086 <george231086@yahoo.co.uk>
2015-06-29 23:28:45 +0100

    Add comments to Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Quadratic {
+/**
+ * 
+ * @author george
+ * 			
+ * 		   Program that asks for a quadratic equation in the form ax^2+bx+c, 
+ *         then returns the roots.
+ * 
+ */
 
+public class Quadratic {
+	// Coefficients of quadratic.
 	private double a;
 	private double b;
 	private double c;
@@ -17,7 +26,7 @@ public class Quadratic {
 		this.c = c;
 	}
 
-	public boolean hasRealSolutions() {
+	public boolean hasRealRoots() {
 		return (b * b - 4 * a * c >= 0);
 	}
 
@@ -49,6 +58,7 @@ public class Quadratic {
 
 		if (isQuad(input)) {
 			a = input.substring(0, input.indexOf("x^2"));
+			// Append 1 to a if poly of form x^2+bx+c.
 			if (a.length() == 0)
 				a = "1";
 			input = input.substring(input.indexOf("x^2") + 3);
@@ -62,9 +72,10 @@ public class Quadratic {
 			throw new IllegalArgumentException();
 
 		String[] coeffs = new String[3];
+		// If poly of form +x^2+x+1 we would have a=+,b=+ so need to append 1.
 		coeffs[0] = (a.endsWith("+") || a.endsWith("-")) ? a + "1" : a;
 		coeffs[1] = (b.endsWith("+") || b.endsWith("-")) ? b + "1" : b;
-		coeffs[2] = (c.endsWith("+") || c.endsWith("-")) ? c + "1" : c;
+		coeffs[2] = c;
 
 		return coeffs;
 	}
@@ -77,14 +88,12 @@ public class Quadratic {
 
 		try {
 			String[] coeffs = parseQuad(scan.nextLine().replaceAll(" +", ""));
-			for (String coeff : coeffs) {
-				System.out.println(coeff);
-			}
 			quad = new Quadratic(Double.parseDouble(coeffs[0]),
 					Double.parseDouble(coeffs[1]),
 					Double.parseDouble(coeffs[2]));
 		} catch (IllegalArgumentException e) {
-			System.out.println("Either input is not a quadratic or in the form ax^2+bx+c!");
+			System.out
+					.println("Either input is not a quadratic or in the form ax^2+bx+c!");
 			System.exit(1);
 		} finally {
 			scan.close();
@@ -94,16 +103,20 @@ public class Quadratic {
 		double x1 = solComponents[0];
 		double x2 = solComponents[1];
 
+		// Would rather output 0 instead of -0
 		if (x1 == -0)
 			x1 = 0;
 
-		if (quad.hasRealSolutions()) {
+		// Find out type of roots and give result.
+		if (quad.hasRealRoots()) {
 
 			if (quad.hasDoubleRoot())
-				System.out.printf("Double root! The solution to 2dp is %.2f\n", x1);
+				System.out.printf("Double root! The solution to 2dp is %.2f\n",
+						x1);
 			else
-				System.out.printf("The solutions to 2dp are:\ns1=%.2f and s2=%.2f\n",
-						x1 + Math.sqrt(x2), x1 - Math.sqrt(x2));
+				System.out.printf(
+						"The solutions to 2dp are:\ns1=%.2f and s2=%.2f\n", x1
+								+ Math.sqrt(x2), x1 - Math.sqrt(x2));
 		} else
 			System.out.printf(
 					"Complex roots! The solutions to 2dp are:\ns1=%1$.2f + i%2$.2f"
